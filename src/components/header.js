161c54f12d455f83ae9eb7412e3d06c4a7f2eb04
Anthony Layne <kepheus.ds@gmail.com>
2022-02-11 15:17:52 -0500

    finished headerAppender Func

--- src/components/header.js
+++ src/components/header.js
@@ -3,21 +3,17 @@
 // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
 // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
 // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-//
-//  <div class="header">
-//    <span class="date">{ date }</span>
-//    <h1>{ title }</h1>
-//    <span class="temp">{ temp }</span>
-//  </div>
-//
 
+/** Creates this markup:
+ ```
+  <div class="header">
+    <span class="date">{ date }</span>
+    <h1>{ title }</h1>
+    <span class="temp">{ temp }</span>
+  </div>
+  ```
+ */
 const Header = (title, date, temp) => {
-  //  <div class="header">
-  //    <span class="date">{ date }</span>
-  //    <h1>{ title }</h1>
-  //    <span class="temp">{ temp }</span>
-  //  </div>
-
   const headerDiv = document.createElement("div");
   const dateSpan = document.createElement("span");
   const titleH1 = document.createElement("h1");
@@ -36,13 +32,18 @@ const Header = (title, date, temp) => {
   return headerDiv;
 };
 
+// TASK 2
+// ---------------------
+// Implement this function taking a css selector as its only argument.
 const headerAppender = (selector) => {
-  // TASK 2
-  // ---------------------
-  // Implement this function taking a css selector as its only argument.
   // It should create a header using the Header component above, passing arguments of your choosing.
-  // It should append the header to the element in the DOM that matches the given selector.
-  //
+  const header = Header("Lambda Times", "02/11/22", "26°");
+
+  // Get the element in the DOM that matches the given selector
+  const element = document.querySelector(selector);
+
+  // Append the header to the element
+  element.append(header);
 };
 
 export { Header, headerAppender };
