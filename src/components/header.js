aac0cac384e0fbc8c4a7caf3058f80be44cf21a5
Anthony Layne <kepheus.ds@gmail.com>
2022-02-11 14:50:47 -0500

    finished Header cmp

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,40 @@
+// TASK 1
+// ---------------------
+// Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
+// The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
+// The text inside elements will be set using their `textContent` property (NOT `innerText`).
+//
+//  <div class="header">
+//    <span class="date">{ date }</span>
+//    <h1>{ title }</h1>
+//    <span class="temp">{ temp }</span>
+//  </div>
+//
+
 const Header = (title, date, temp) => {
-  // TASK 1
-  // ---------------------
-  // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
   //  <div class="header">
   //    <span class="date">{ date }</span>
   //    <h1>{ title }</h1>
   //    <span class="temp">{ temp }</span>
   //  </div>
-  //
-}
+
+  const headerDiv = document.createElement("div");
+  const dateSpan = document.createElement("span");
+  const titleH1 = document.createElement("h1");
+  const tempSpan = document.createElement("span");
+
+  headerDiv.classList.add("header");
+  dateSpan.classList.add("date");
+  tempSpan.classList.add("temp");
+
+  dateSpan.textContent = date;
+  titleH1.textContent = title;
+  tempSpan.textContent = temp;
+
+  headerDiv.append(dateSpan, titleH1, tempSpan);
+
+  return headerDiv;
+};
 
 const headerAppender = (selector) => {
   // TASK 2
@@ -20,6 +43,6 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
-}
+};
 
-export { Header, headerAppender }
+export { Header, headerAppender };
