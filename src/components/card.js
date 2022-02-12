76be4ad91c7492e0844c9d359fab6324cb0f13da
Anthony Layne <kepheus.ds@gmail.com>
2022-02-11 19:28:30 -0500

    finished cards

--- src/components/card.js
+++ src/components/card.js
@@ -1,11 +1,13 @@
-const Card = (article) => {
-  // TASK 5
-  // ---------------------
-  // Implement this function, which should return the markup you see below.
-  // It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
+import axios from "axios";
+
+// TASK 5
+// ---------------------
+// Implement this function, which should return the markup you see below.
+
+// It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
+const Card = ({ headline, authorPhoto, authorName }) => {
+  // The tags used, the hierarchy of elements, and their attributes must match the provided markup exactly!
   // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
   //
   // <div class="card">
   //   <div class="headline">{ headline }</div>
@@ -16,18 +18,56 @@ const Card = (article) => {
   //     <span>By { authorName }</span>
   //   </div>
   // </div>
-  //
-}
 
-const cardAppender = (selector) => {
-  // TASK 6
-  // ---------------------
-  // Implement this function that takes a css selector as its only argument.
-  // It should obtain articles from this endpoint: `http://localhost:5000/api/articles` (test it in Postman/HTTPie!).
-  // However, the articles do not come organized in a single, neat array. Inspect the response closely!
-  // Create a card from each and every article object in the response, using the Card component.
+  const cardDiv = document.createElement("div");
+  const headlineDiv = document.createElement("div");
+  const authorDiv = document.createElement("div");
+  const imgContainerDiv = document.createElement("div");
+  const authorPhotoImg = document.createElement("img");
+  const authorNameSpan = document.createElement("span");
+
+  cardDiv.classList.add("card");
+  headlineDiv.classList.add("headline");
+  authorDiv.classList.add("author");
+  imgContainerDiv.classList.add("img-container");
+
+  headlineDiv.textContent = headline;
+  authorPhotoImg.src = authorPhoto;
+  authorNameSpan.textContent = authorName;
+
+  imgContainerDiv.appendChild(authorPhotoImg);
+  authorDiv.append(imgContainerDiv, authorNameSpan);
+  cardDiv.append(headlineDiv, authorDiv);
+
+  // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
+  cardDiv.addEventListener("click", () => console.log(headline));
+
+  return cardDiv;
+};
+
+// TASK 6
+// ---------------------
+// Implement this function that takes a css selector as its only argument.
+const cardAppender = async (selector) => {
   // Append each card to the element in the DOM that matches the selector passed to the function.
-  //
-}
+  const element = document.querySelector(selector);
 
-export { Card, cardAppender }
+  // It should obtain articles from this endpoint: `http://localhost:5000/api/articles` (test it in Postman/HTTPie!).
+  try {
+    // However, the articles do not come organized in a single, neat array. Inspect the response closely!
+    const response = await axios.get("http://localhost:5000/api/articles");
+    const categories = response.data.articles;
+
+    // Create a card from each and every article object in the response, using the Card component:
+    Object.values(categories).forEach((category) => {
+      category.forEach((article) => {
+        const card = Card(article);
+        element.append(card);
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { Card, cardAppender };
